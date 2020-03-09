7f99aa588f651b838e241a7c123888b7501697fb
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-09 20:51:55 -0300

    resolucao do problema

--- src/br/fai/collections01/Main.java
+++ src/br/fai/collections01/Main.java
@@ -13,20 +13,43 @@ public class Main {
 
 	}
 	private Scanner scanner = new Scanner(System.in);
-	
+	private final String VALOR_INVALIDO="VALOR_INVALIDO";
 	private void start() {
 		List <String> lista = new ArrayList<String>();
 		
-		String valorRecebido= obterDados();
-		
-			System.out.println("O valor receido foi: "
-			+valorRecebido);	
+			
+			while(lista.size()<5) {
+				String valorRecebido= obterDados();
+				
+				//System.out.println("O valor recebido foi: "+valorRecebido);
+				if(valorRecebido.isEmpty()
+					|| valorRecebido.equals (VALOR_INVALIDO)) {
+					System.out.println("Este valor foi descartado");
+				}
+				else {
+					lista.add(valorRecebido);
+				}
+				
+			}
+			System.out.println("---------------");
+			for (String item : lista) {
+				System.out.println("O item da lista eh: "+ item);
+			}
 	}
 	
 	private String obterDados() {
-		System.out.println("Digite o valor: ");
-		int valor= scanner.nextInt();
-		return String.valueOf(valor);
+		try {
+			System.out.println("Digite o valor: ");
+			int valor= scanner.nextInt();
+			
+			return String.valueOf(valor);
+		} catch (Exception e) {
+			scanner.next();
+			System.out.println("Um valor invalido foi digitado");
+		return VALOR_INVALIDO;
+		
+		}
+		
 	}
 
 }
